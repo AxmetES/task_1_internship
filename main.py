9687d55e728ab99f3dae525b0e33a5f2b55c4ec1
axmetes <fraktsia@gmail.com>
2024-02-28 21:41:26 +0600

    Fix parse of all entries.

--- main.py
+++ main.py
@@ -177,16 +177,16 @@ if __name__ == '__main__':
         format="%(asctime)s - %(levelname)s - %(message)s",
         encoding="utf-8",
     )
-    # main()
-    main_url = 'https://shop.kz/'
-    s = requests.Session()
-    s.headers.update({
-        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
-        'Accept-Encoding': 'gzip, deflate, br, zstd',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Cache-Control': 'no-cache',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
-    })
-    sub_sub_catalogs_urls = ['https://shop.kz/offers/naushniki-i-garnitury/naushniki-dlya-telefonov/']
+    main()
+    # main_url = 'https://shop.kz/'
+    # s = requests.Session()
+    # s.headers.update({
+    #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    #     'Accept-Encoding': 'gzip, deflate, br, zstd',
+    #     'Accept-Language': 'en-US,en;q=0.9',
+    #     'Cache-Control': 'no-cache',
+    #     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+    # })
+    # sub_sub_catalogs_urls = ['https://shop.kz/offers/naushniki-i-garnitury/naushniki-dlya-telefonov/']
     # sub_sub_catalogs_urls = read_from_file('catalogs_urls.json')
-    get_catalog_products(s, sub_sub_catalogs_urls, main_url)
+    # get_catalog_products(s, sub_sub_catalogs_urls, main_url)
